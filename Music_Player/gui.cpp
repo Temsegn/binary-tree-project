7039ba9181aa40f60ffb753beb84a5a4998e0127
tomtech <tommr2323@gmail.com>
2024-03-25 22:09:48 +0300

    add gui and some changes

---
+++ Music_Player/gui.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+int main() {
+	 cout<<"****** LET'S ENJOY WITH MUSIC******"<<endl<<endl;
+      cout << "      __|__|__|" <<  endl;
+     cout << "      /         \\" <<  endl;
+     cout << "     /           \\" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |___|_____|___|" <<  endl;
+     cout << "   /|___|_____|___|\\" <<  endl;
+     cout << "  / |___|_____|___| \\" <<  endl;
+     cout << " /  |___|_____|___|  \\" <<  endl;
+     cout << "/   |___|_____|___|   \\" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout <<"/                       \ "<< endl;
+     cout<<" -------------------------"<<endl;
+     cout<<"|-------------------------|"<<endl;
+     cout<<"---------------------------"<<endl;
+     cout<<"---------------------------"<<endl;
+
+    return 0;
+}
---
+++ Music_Player/gui.hpp
@@ -0,0 +1,62 @@
+#include <iostream>
+class GUI {
+public:
+    void someFunction()
+	 {
+	 	 cout<<"****** LET'S ENJOY WITH MUSIC******"<<endl<<endl;
+      cout << "      __|__|__|" <<  endl;
+     cout << "      /         \\" <<  endl;
+     cout << "     /           \\" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |   |     |   |" <<  endl;
+     cout << "    |___|_____|___|" <<  endl;
+     cout << "   /|___|_____|___|\\" <<  endl;
+     cout << "  / |___|_____|___| \\" <<  endl;
+     cout << " /  |___|_____|___|  \\" <<  endl;
+     cout << "/   |___|_____|___|   \\" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |___|_____|___|   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|   |   |     |   |   |" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout << "|___|___|_____|___|___|" <<  endl;
+     cout <<"/                       \ "<< endl;
+     cout<<" -------------------------"<<endl;
+     cout<<"|-------------------------|"<<endl;
+     cout<<"---------------------------"<<endl;
+     cout<<"---------------------------"<<endl;
+
+    return ;
+	 }
+};
+
+ 
